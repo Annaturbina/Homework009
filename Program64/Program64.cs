385e624d80e58d137f2c325dd746eaaab2d593a1
Anna <turbina.anna97@yandex.ru>
2022-12-20 18:54:03 +0100

    new folder

---
+++ Program64/Program64.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int NumbersSequence(int N)
+{
+    while (N > 1)
+    {
+        Console.Write($"{N} ");
+        N--;
+    }
+    return N;
+}
+Console.WriteLine(NumbersSequence(N));
---
+++ Program66/Program66.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N (должно быть больше M)");
+int n = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int m, int n)
+{
+    int sum = 0;
+    while(m <= n)
+    {
+        sum += m;
+        m++;
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} = {SumNumbers(m, n)}");
